b09433aed729bb9600ef32ff4882d021109f8013
Sandro Sales <sandrofsales@gmail.com>
2015-12-02 14:54:07 -0300

    laravel_commerce-Checkout03

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Category;
+use CodeCommerce\Events\CheckoutEvent;
+use CodeCommerce\Order;
+use CodeCommerce\OrderItem;
+use Illuminate\Http\Request;
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+
+class CheckoutController extends Controller
+{
+
+    public function __construct ()
+    {
+        //$this->middleware('auth');
+
+    }
+
+
+    public function place(Order $orderModel, OrderItem $orderItem)
+    {
+        if(!Session::has('cart')){
+            return false;
+
+        }
+
+        $cart = Session::get('cart');
+
+        if($cart->getTotal() > 0){
+
+            $order = $orderModel->create(['user_id'=>Auth::user()->id, 'total'=>$cart->getTotal()]);
+
+            foreach ($cart->all() as $k=>$item){
+
+                $order->items()->create(['product_id'=>$k, 'price'=>$item['price'], 'qtd'=>$item['qtd']]);
+            }
+
+            $cart->clear();
+            event(new CheckoutEvent(Auth::user(), $order));
+
+            return view('store.checkout',compact('order','cart'));
+
+        }
+        $categories = Category::all();
+        return view('store.checkout',compact('order'), ['cart'=>'empty','categories'=>$categories]);
+
+
+
+    }
+}
